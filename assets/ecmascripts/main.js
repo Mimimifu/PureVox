508e702689f0caa215498d47b3abab924a8e157c
Mimimifu <130190513+Mimimifu@users.noreply.github.com>
2023-04-14 19:29:19 -0300

    navCheck

--- assets/ecmascripts/main.js
+++ assets/ecmascripts/main.js
@@ -1,5 +1,30 @@
-'use strict'
+'use strict';
 
 class main {
-    
+
+    constructor(){
+
+    }
+
+}
+
+class navCheck {
+
+    //openai script
+
+    navigation(){
+        if (/Edge\/\d./i.test(navigator.userAgent)) {
+            console.log("Microsoft Edge - versão do ECMAScript 6+");
+          } else if (/Chrome\/\d./i.test(navigator.userAgent)) {
+            console.log("Google Chrome - versão do ECMAScript 6+");
+          } else if (/Firefox\/\d./i.test(navigator.userAgent)) {
+            console.log("Mozilla Firefox - versão do ECMAScript 6+");
+          } else if (/Safari\/\d./i.test(navigator.userAgent)) {
+            console.log("Apple Safari - versão do ECMAScript 6+");
+          } else if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+            console.log("Microsoft Internet Explorer - versão anterior ao ECMAScript 6");
+          } else {
+            console.log("Outros navegadores - versão desconhecida");
+          }
+    }
 }
