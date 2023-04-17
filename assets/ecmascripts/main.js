70cfb0ccefd13b42d3b4a8dc9bd516b661b4c0a7
Mimimifu <130190513+Mimimifu@users.noreply.github.com>
2023-04-17 12:46:28 +0000

    assets/ecmascripts/main:

--- assets/ecmascripts/main.js
+++ assets/ecmascripts/main.js
@@ -4,20 +4,21 @@ class main {
 
 
     constructor(){
-      this._nav = new navCheck();
+      let u = new utils();
+      u.navigation();
     }
 }
 
-class navCheck {
+class utils {
 
 
     constructor(){
-      this.navigation();
+
     }
 
     navigation(){
         //openai script 0.0.1
-        if (/Edge\/\d./i.test(navigator.userAgent)) {
+        if (/Edg\/\d./i.test(navigator.userAgent)) {
             console.log("Microsoft Edge - versão do ECMAScript 6+");
           } else if (/Chrome\/\d./i.test(navigator.userAgent)) {
             console.log("Google Chrome - versão do ECMAScript 6+");
@@ -31,6 +32,32 @@ class navCheck {
             console.log("Outros navegadores - versão desconhecida");
           }
     }
+
+    stateNow(){
+
+      
+    }
+    
+    callback(mutationList, observer) {
+      mutationList.forEach((mutation) => {
+        switch (mutation.type) {
+          case "childList":
+            /* One or more children have been added to and/or removed
+               from the tree.
+               (See mutation.addedNodes and mutation.removedNodes.) */
+            break;
+          case "attributes":
+            /* An attribute value changed on the element in
+               mutation.target.
+               The attribute name is in mutation.attributeName, and
+               its previous value is in mutation.oldValue. */
+            break;
+        }
+      });
+    }
+
+
+
 }
 
 class protoTypes {
@@ -65,5 +92,5 @@ new main();
 console.log('ue');
 let b = new protoTypes(function(name = 'name'){return name},'opa');
 console.log(b);
-let c = new protoTypes(function(name = 'outro2'){ name},'outro');
+let c = new protoTypes(function(name = 'outro2'){ name},'outro2');
 console.log(c);
