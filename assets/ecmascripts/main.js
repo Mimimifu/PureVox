42ad177e59a67611c18a3233252d86b2a25e4929
Mimimifu <130190513+Mimimifu@users.noreply.github.com>
2023-04-24 19:34:39 -0300

    update

--- assets/ecmascripts/main.js
+++ assets/ecmascripts/main.js
@@ -7,7 +7,12 @@ class main {
     constructor(){
       let u = new utils();
       let g = u.objectGlobal();
+      
       console.log(g);
+      let el = u.createEl('input');
+      g.document.body.append(el);
+      console.log(g.document.documentElement.children.item('body'));
+      u.addEvent('input','click',() =>{(confirm('ok')? console.log('ok'): console.log('cancel'))})
       u.navigation();
       u.title('Main');
       u.changeText('h1','<h2>text</h2>');
@@ -17,6 +22,8 @@ class main {
       u.changeText('input[type=text]','test');
       console.log(u.sel('input[type=text]'));
       console.log(g.btoa('test'));
+      console.log(Event.target);
+
 
     }
 
--- assets/ecmascripts/utils.js
+++ assets/ecmascripts/utils.js
@@ -1,27 +1,15 @@
 'use strict';
 
+
 class utils {
 
-    
+
     constructor(){
       //const og = this.objectGlobal();
     }
 
-    navigation(){
-        //openai script 0.0.1
-        if (/Edg\/\d./i.test(navigator.userAgent)) {
-            console.log("Microsoft Edge - versão do ECMAScript 6+");
-          } else if (/Chrome\/\d./i.test(navigator.userAgent)) {
-            console.log("Google Chrome - versão do ECMAScript 6+");
-          } else if (/Firefox\/\d./i.test(navigator.userAgent)) {
-            console.log("Mozilla Firefox - versão do ECMAScript 6+");
-          } else if (/Safari\/\d./i.test(navigator.userAgent)) {
-            console.log("Apple Safari - versão do ECMAScript 6+");
-          } else if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
-            console.log("Microsoft Internet Explorer - versão anterior ao ECMAScript 6");
-          } else {
-            console.log("Outros navegadores - versão desconhecida");
-          }
+    createEl(tag){
+      return document.createElement(tag);
     }
   
     sel(selector){
@@ -58,6 +46,18 @@ class utils {
 
     changeValue(selector, value){
       this.sel(selector).value = value;
+    }
+
+    addClass(seletor,name){
+      this.sel(seletor).classList.add(...name);
+    }
+
+    changeClass(seletor,name){
+      this.sel(seletor).classList.replace(...name);
+    }
+    
+    delClass(seletor,name){
+      this.sel(seletor).classList.remove(...name);
     }
 
     addAttibute(selector,nameAttribute,value){
@@ -101,6 +101,69 @@ class utils {
         throw new Error('| Onde é que tô ? | Where am I? | Wo bin ich? | Où suis-je? | ここはどこですか。| koko wa doko desu ka . | Где я? | gde ya? | Де я? | day yo? | 제가 있는 곳은 어디입니까? | jega issneun goseun eodiipnikka? |我在哪裡？| wǒ zàinǎli? |');
       }
     }
+
+
+    getECMAScript() {
+      // reference https://stackoverflow.com/questions/47374678/how-to-detect-ecmascript-version Infigon
+      let array = []
+      if (!Array.isArray) {
+        return 3
+      } else if (!window.Promise) {
+        return 5
+      } else if (!array.includes) {
+        return 6
+      } else if (!"".padStart) {
+        return 7
+      } else if (!Promise.prototype.finally) {
+        return 8
+      } else if (!window.BigInt) {
+        return 9
+      } else if (!Promise.allSettled) {
+        return 10
+      } else if (!"".replaceAll) {
+        return 11
+      } else if (!array.at) {
+        return 12
+      } else {
+        return 13
+      }
+    }
+
+    getECMAScriptToYear(){
+      // reference https://stackoverflow.com/questions/47374678/how-to-detect-ecmascript-version Infigon
+      let ecmascript = this.getECMAScript();
+      let year = 1999;
+      if(ecmascript !== 3){
+        year = ecmascript + 2009;
+      }
+      return `Version ECMAScript 6+ (${ecmascript} Current), Year ${year}`;
+    }
+
+    navigation(){
+      //openai script 0.0.1
+      if (/Edg\/\d./i.test(navigator.userAgent)) {
+          //console.log("Microsoft Edge - versão do ECMAScript 6+");
+          console.log(`Microsoft Edge - ${this.getECMAScriptToYear()}`);
+          //this.getECMAScriptToYear();
+        } else if (/Chrome\/\d./i.test(navigator.userAgent)) {
+          //console.log("Google Chrome - versão do ECMAScript 6+");
+          console.log(`Google Chrome - ${this.getECMAScriptToYear()}`);
+        } else if (/Firefox\/\d./i.test(navigator.userAgent)) {
+          //console.log("Mozilla Firefox - versão do ECMAScript 6+");
+          console.log(`Mozilla Firefox - ${this.getECMAScriptToYear()}`);
+        } else if (/Safari\/\d./i.test(navigator.userAgent)) {
+          //console.log("Apple Safari - versão do ECMAScript 6+");
+          console.log(`Apple Safari - ${this.getECMAScriptToYear()}`);
+        } else if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+          //console.log("Microsoft Internet Explorer - versão anterior ao ECMAScript 6");
+          console.log(`Microsoft Internet Explorer - ${this.getECMAScriptToYear()}`);
+        } else {
+          //console.log("Outros navegadores - versão desconhecida");
+          console.log(`Another Browser - ${this.getECMAScriptToYear()}`);
+        }
+    }
+
+    
 
 }
 
