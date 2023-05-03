4e2b24c4114dc008e6d26cc29af7f9fea680fdd3
Mimimifu <130190513+Mimimifu@users.noreply.github.com>
2023-05-03 20:31:06 -0300

    update

--- assets/ecmascripts/main.js
+++ assets/ecmascripts/main.js
@@ -8,22 +8,22 @@ class main {
       let u = new utils();
       let g = u.objectGlobal();
       
-      console.log(g);
-      let el = u.createEl('input');
-      el.setAttribute('name','hello world');
-      u.saveEl(el);
-      //console.log(g.document.documentElement.children.item('body'));
-      u.addEvent('input','click',() =>{(confirm('ok')? console.log('ok'): console.log('cancel'))})
-      u.navigation();
-      u.title('Main');
-      u.changeText('h1','<h2>text</h2>');
-      console.log(u.sel('h1'));
-      u.changeHTML('application-root','<h2>text</h2>');
-      console.log(u.sel('application-root'));
-      u.changeText('input[type=text]','test');
-      console.log(u.sel('input[type=text]'));
-      //console.log(g.btoa('test'));
-      console.log(Event.target);
+      // console.log(g);
+      // let el = u.createEl('input');
+      // el.setAttribute('name','hello world');
+      // u.saveEl(el);
+      // //console.log(g.document.documentElement.children.item('body'));
+      // u.addEvent('input','click',() =>{(confirm('ok')? console.log('ok'): console.log('cancel'))})
+      // u.navigation();
+      // u.title('Main');
+      // u.changeText('h1','<h2>text</h2>');
+      // console.log(u.sel('h1'));
+      // u.changeHTML('application-root','<h2>text</h2>');
+      // console.log(u.sel('application-root'));
+      // u.changeText('input[type=text]','test');
+      // console.log(u.sel('input[type=text]'));
+      // //console.log(g.btoa('test'));
+      // console.log(Event.target);
 
 
     }
